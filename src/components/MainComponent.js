d05581e83930f839d3418acd9361df82371fc8fc
Pardeep Gahlawat <gahlawat80@hotmail.com>
2021-10-16 13:46:51 -0400

    added contactus component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,10 @@ import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import Contact from './ContactusComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+
+
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -46,6 +50,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path="/directory/:directoryid" component={CampsiteWithId} />
                     <Route path="/contactus" component={Contact} />
+                    <Route path="/aboutus" render={()=> <About partners={this.state.partners}/>} />
                     <Redirect to='/home' />
                     
                 </Switch>
