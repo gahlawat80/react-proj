7e0975e0e2027b3fccab958d2100172e2c75372c
Pardeep Gahlawat <gahlawat80@hotmail.com>
2021-10-28 15:52:25 -0400

    Redux Thunk

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderCampsiteCard({site}){
     return(
@@ -15,14 +16,36 @@ function RenderCampsiteCard({site}){
     );
 }
 
-function Directory({campsites}){
-        const directories = campsites.map(site =>{
-            return (
-                <div className="col-md-5 m-1" key={site.id}>
-                    <RenderCampsiteCard site={site}/>
+function Directory(props){
+        const directory = props.campsites.campsites.map(campsite => {
+            console.log(campsite);
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderCampsiteCard site={campsite} />
+            </div>
+        );
+    });
+
+    if (props.campsites.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
-            );
-        })
+            </div>
+        );
+    }
+    if (props.campsites.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.campsites.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
         return (
             <div className="container">
                 <div className="row">
@@ -36,7 +59,7 @@ function Directory({campsites}){
                     </div>
                 </div>
                 <div className="row">
-                    {directories}
+                    {directory}
                 </div>                
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,10 +9,11 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchCampsites } from '../redux/ActionCreators';
 
 const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    fetchCampsites: () => (fetchCampsites())
 };
 
 const mapStateToProps = state => {
@@ -26,19 +27,33 @@ const mapStateToProps = state => {
 
 class Main extends Component {
 
+    componentDidMount() {
+        this.props.fetchCampsites();
+    }
+
     render() {
+
         const HomePage = () => {
             return (
-                <Home campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
-                promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.props.partners.filter(partner => partner.featured)[0]} />
+                <Home
+                    campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    campsitesLoading={this.props.campsites.isLoading}
+                    campsitesErrMess={this.props.campsites.errMess}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
+                />
             );
-        };
+        }
+
         const CampsiteWithId = ({match}) => {
+            console.log(this.props.campsites.campsites)
+            console.log(match.params.campsiteId)
             return (
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.directoryid)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.directoryid)}
+                    campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    isLoading={this.props.campsites.isLoading}
+                    errMess={this.props.campsites.errMess}
+                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                     addComment={this.props.addComment}
                 />
             );
@@ -49,7 +64,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                    <Route path="/directory/:directoryid" component={CampsiteWithId} />
+                    <Route path="/directory/:campsiteId" component={CampsiteWithId} />
                     <Route path="/contactus" component={Contact} />
                     <Route path="/aboutus" render={()=> <About partners={this.props.partners}/>} />
                     <Redirect to='/home' />
